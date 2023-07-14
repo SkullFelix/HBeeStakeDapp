6ee63a9a4e5193e03eedb4acae4def6cf25a3127
SkullFelix <125965571+SkullFelix@users.noreply.github.com>
2023-07-14 19:17:15 +0200

    Update stake.tsx
    reward x time

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -76,8 +76,8 @@ const Stake: NextPage = () => {
         <>
           <h2>Your Honey</h2>
           <p className={styles.selectBoxDescription}>
-             <b>7 Days Stake</b>{" "}
-            and receive <b>1 HoneyBees</b> .
+             <b>24h Stake</b>{" "}
+            and receive <b>10 HoneyBees</b>.
           </p>
           <div className={styles.tokenGrid}>
             <div className={styles.tokenItem}>
